2e45ca50e33bce3b4cccfd941a1369b002456673
hangjay <110868703+hangjay@users.noreply.github.com>
2023-01-14 11:17:56 -0600

    fix stuff of stuff

--- src/main/java/frc/robot/commands/ClawCommand.java
+++ src/main/java/frc/robot/commands/ClawCommand.java
@@ -31,7 +31,6 @@ public class ClawCommand extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    Boolean clawClosed = true;
 
     if (this.controller.getAButton()&&aPressed == false){
       aPressed = true;
