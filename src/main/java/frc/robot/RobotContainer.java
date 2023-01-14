24d3a48e761faa04d883bbcc9644c06918dfbffa
DaneWenzel <dane.wenzel699@gsdstudents.org>
2023-01-14 12:58:41 -0600

    Set up robot container.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,6 +4,19 @@
 
 package frc.robot;
 import frc.robot.Constants.OperatorConstants;
+import frc.robot.commands.ArmExtensionCommand;
+import frc.robot.commands.ArmPivotCommand;
+import frc.robot.commands.ClawCommand;
+import frc.robot.commands.ElevatorCommand;
+import frc.robot.commands.RotateTurret;
+import frc.robot.commands.TankDrive;
+import frc.robot.subsystems.ArmExtension;
+import frc.robot.subsystems.ArmPivot;
+import frc.robot.subsystems.Claw;
+import frc.robot.subsystems.DriveTrain;
+import frc.robot.subsystems.Elevator;
+import frc.robot.subsystems.Turret;
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
@@ -16,6 +29,26 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
  */
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
+  // Subsystems
+  private ArmExtension armExtension = new ArmExtension();
+  private ArmPivot armPivot = new ArmPivot();
+  private Claw claw = new Claw();
+  private DriveTrain driveTrain = new DriveTrain();
+  private Elevator elevator = new Elevator();
+  private Turret turret = new Turret();
+
+  //Joysticks
+  private Joystick joystick_left = new Joystick(0);
+  private Joystick joystick_right = new Joystick(1);
+  private Joystick secondarycontroller = new Joystick(2);
+
+  //Commands
+  private ArmExtensionCommand armExtensionCommand = new ArmExtensionCommand(armExtension, secondarycontroller);
+  private ArmPivotCommand armPivotCommand = new ArmPivotCommand(armPivot, secondarycontroller);
+  private ClawCommand clawCommand = new ClawCommand(claw, secondarycontroller);
+  private ElevatorCommand elevatorCommand = new ElevatorCommand(elevator, secondarycontroller);
+  private RotateTurret rotateTurret = new RotateTurret(turret, secondarycontroller);
+  private TankDrive tankDrive = new TankDrive(driveTrain, joystick_left, joystick_right);
 
   // Replace with CommandPS4Controller or CommandJoystick if needed
   private final CommandXboxController m_driverController =
@@ -38,7 +71,12 @@ public class RobotContainer {
    */
   private void configureBindings() {
     // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
-
+    armExtension.setDefaultCommand(armExtensionCommand);
+    armPivot.setDefaultCommand(armPivotCommand);
+    claw.setDefaultCommand(clawCommand);
+    elevator.setDefaultCommand(elevatorCommand);
+    turret.setDefaultCommand(rotateTurret);
+    driveTrain.setDefaultCommand(tankDrive);
     // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
     // cancelling on release.
   }
