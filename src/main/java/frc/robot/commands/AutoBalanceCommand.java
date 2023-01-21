bcc7fdcbd869feaa4fbf8c3abe418598a46cf79a
hangjay <110868703+hangjay@users.noreply.github.com>
2023-01-21 09:48:31 -0600

    added PID part 2

--- src/main/java/frc/robot/commands/AutoBalanceCommand.java
+++ src/main/java/frc/robot/commands/AutoBalanceCommand.java
@@ -6,16 +6,23 @@ package frc.robot.commands;
 
 import com.kauailabs.navx.frc.AHRS;
 
+import edu.wpi.first.networktables.GenericEntry;
 import edu.wpi.first.wpilibj.SPI;
+import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.helper.PID;
 import frc.robot.subsystems.DriveTrain;
 
 public class AutoBalanceCommand extends CommandBase {
   /** Creates a new AutoBalanceCommand. */
   AHRS gyro = new AHRS(SPI.Port.kMXP);
+  PID controller = new PID (0,0,0);
   DriveTrain DT = null;
+  GenericEntry pEntry = Shuffleboard.getTab("AutoMenu").add("p",0).withWidget(BuiltInWidgets.kTextView).getEntry();
+  GenericEntry iEntry = Shuffleboard.getTab("AutoMenu").add("i",0).withWidget(BuiltInWidgets.kTextView).getEntry();
+  GenericEntry dEntry = Shuffleboard.getTab("AutoMenu").add("d",0).withWidget(BuiltInWidgets.kTextView).getEntry();
   public AutoBalanceCommand(DriveTrain DT) {
     // Use addRequirements() here to declare subsystem dependencies.
     this.DT = DT;
@@ -26,19 +33,20 @@ public class AutoBalanceCommand extends CommandBase {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
+    controller.reset();
+    
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
+    controller.kp = pEntry.getDouble(0);
+    controller.ki = iEntry.getDouble(0);
+    controller.kd = dEntry.getDouble(0);
     float pitch = gyro.getRoll();
     SmartDashboard.putNumber("Pitch", pitch);
-    if (pitch > 0){
-      DT.tankdrive(-0.1,-0.1);
-    }
-    else{
-      DT.tankdrive(0.1, 0.1);
-    }
+    double speed = Math.max(controller.step(pitch), 0.3);
+    DT.tankdrive(speed, speed);
   }
 
   // Called once the command ends or is interrupted.
