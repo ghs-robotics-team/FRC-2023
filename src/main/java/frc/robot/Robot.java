15bfd5d6a3078fe334f0aa62faedeff0b542fcba
Awesomeness278 <tylerbalota@gmail.com>
2023-03-01 19:05:46 -0600

    Auto functions are ready, set points are ready but need to be set

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -10,6 +10,7 @@ import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.robot.helper.AutoType;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
